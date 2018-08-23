d918746cc7e47263e14ca7c98e461e9492227f9f
wuhongchuan1987 <1559425303@qq.com>
2018-08-23 17:12:50 +0800

    update opt file

--- sgame_opt_file.sh
+++ sgame_opt_file.sh
@@ -1,5 +1,9 @@
 #201  big35short whc
 
+#首次
+git init
+git clone git@35.160.46.145:/home/git/deploy.git
+
 #提交代码
 cd /e/WARSHIPS/my_git
 #add
@@ -8,7 +12,13 @@ git add .
 git commit -m 'sgame_opt_file.sh更新提交'
 #正式提交
 git push -u origin master
-
-
 #拉取代码到本地（pull）
 git pull
+git pull [remote] [branch]
+
+#正式环境
+git@35.160.46.145:/home/git/deploy.git
+#add_key
+/home/git/.ssh/authorized_keys
+
+#git config --global color.ui true
